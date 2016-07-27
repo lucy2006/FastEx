db1e8e35b237ca38bcf30c3fad9ee1652fa68c5f
KevinBarker <st_jimmy21@hotmail.co.uk>
2016-07-27 13:23:51 -0400

    testtesttest
    lablblalsasd

--- FinalProject/FinalProject/Form1.Designer.cs
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -68,200 +68,200 @@
             // lblTrackNum
             // 
             this.lblTrackNum.AutoSize = true;
-            this.lblTrackNum.Location = new System.Drawing.Point(126, 220);
+            this.lblTrackNum.Location = new System.Drawing.Point(110, 206);
             this.lblTrackNum.Name = "lblTrackNum";
-            this.lblTrackNum.Size = new System.Drawing.Size(231, 32);
+            this.lblTrackNum.Size = new System.Drawing.Size(200, 29);
             this.lblTrackNum.TabIndex = 0;
             this.lblTrackNum.Text = "Tracking Number";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 38);
+            this.button1.Location = new System.Drawing.Point(82, 36);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(643, 133);
+            this.button1.Size = new System.Drawing.Size(563, 124);
             this.button1.TabIndex = 1;
             this.button1.Text = "Picture Place holder";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // txtTrackNum
             // 
-            this.txtTrackNum.Location = new System.Drawing.Point(121, 283);
+            this.txtTrackNum.Location = new System.Drawing.Point(106, 265);
             this.txtTrackNum.Name = "txtTrackNum";
-            this.txtTrackNum.Size = new System.Drawing.Size(501, 38);
+            this.txtTrackNum.Size = new System.Drawing.Size(439, 35);
             this.txtTrackNum.TabIndex = 2;
             // 
             // lblShipdate
             // 
             this.lblShipdate.AutoSize = true;
-            this.lblShipdate.Location = new System.Drawing.Point(126, 345);
+            this.lblShipdate.Location = new System.Drawing.Point(110, 323);
             this.lblShipdate.Name = "lblShipdate";
-            this.lblShipdate.Size = new System.Drawing.Size(203, 32);
+            this.lblShipdate.Size = new System.Drawing.Size(171, 29);
             this.lblShipdate.TabIndex = 3;
             this.lblShipdate.Text = "Shipment Date";
             // 
             // txtShipDate
             // 
-            this.txtShipDate.Location = new System.Drawing.Point(121, 398);
+            this.txtShipDate.Location = new System.Drawing.Point(106, 372);
             this.txtShipDate.Name = "txtShipDate";
-            this.txtShipDate.Size = new System.Drawing.Size(495, 38);
+            this.txtShipDate.Size = new System.Drawing.Size(434, 35);
             this.txtShipDate.TabIndex = 4;
             // 
             // lblDestination
             // 
             this.lblDestination.AutoSize = true;
-            this.lblDestination.Location = new System.Drawing.Point(126, 471);
+            this.lblDestination.Location = new System.Drawing.Point(110, 441);
             this.lblDestination.Name = "lblDestination";
-            this.lblDestination.Size = new System.Drawing.Size(159, 32);
+            this.lblDestination.Size = new System.Drawing.Size(133, 29);
             this.lblDestination.TabIndex = 5;
             this.lblDestination.Text = "Destination";
             // 
             // txtDestination
             // 
-            this.txtDestination.Location = new System.Drawing.Point(121, 531);
+            this.txtDestination.Location = new System.Drawing.Point(106, 497);
             this.txtDestination.Name = "txtDestination";
-            this.txtDestination.Size = new System.Drawing.Size(495, 38);
+            this.txtDestination.Size = new System.Drawing.Size(434, 35);
             this.txtDestination.TabIndex = 6;
             // 
             // lblESTofDeliv
             // 
             this.lblESTofDeliv.AutoSize = true;
-            this.lblESTofDeliv.Location = new System.Drawing.Point(126, 609);
+            this.lblESTofDeliv.Location = new System.Drawing.Point(110, 570);
             this.lblESTofDeliv.Name = "lblESTofDeliv";
-            this.lblESTofDeliv.Size = new System.Drawing.Size(207, 32);
+            this.lblESTofDeliv.Size = new System.Drawing.Size(177, 29);
             this.lblESTofDeliv.TabIndex = 7;
             this.lblESTofDeliv.Text = "EST of delivery";
             // 
             // txtESTofDeliv
             // 
-            this.txtESTofDeliv.Location = new System.Drawing.Point(121, 674);
+            this.txtESTofDeliv.Location = new System.Drawing.Point(106, 631);
             this.txtESTofDeliv.Name = "txtESTofDeliv";
-            this.txtESTofDeliv.Size = new System.Drawing.Size(489, 38);
+            this.txtESTofDeliv.Size = new System.Drawing.Size(428, 35);
             this.txtESTofDeliv.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(126, 756);
+            this.label5.Location = new System.Drawing.Point(110, 707);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 32);
+            this.label5.Size = new System.Drawing.Size(0, 29);
             this.label5.TabIndex = 9;
             // 
             // txtCurrentLoc
             // 
-            this.txtCurrentLoc.Location = new System.Drawing.Point(121, 812);
+            this.txtCurrentLoc.Location = new System.Drawing.Point(106, 760);
             this.txtCurrentLoc.Name = "txtCurrentLoc";
-            this.txtCurrentLoc.Size = new System.Drawing.Size(495, 38);
+            this.txtCurrentLoc.Size = new System.Drawing.Size(434, 35);
             this.txtCurrentLoc.TabIndex = 10;
             // 
             // lblExprShip
             // 
             this.lblExprShip.AutoSize = true;
-            this.lblExprShip.Location = new System.Drawing.Point(126, 905);
+            this.lblExprShip.Location = new System.Drawing.Point(110, 847);
             this.lblExprShip.Name = "lblExprShip";
-            this.lblExprShip.Size = new System.Drawing.Size(245, 32);
+            this.lblExprShip.Size = new System.Drawing.Size(208, 29);
             this.lblExprShip.TabIndex = 11;
             this.lblExprShip.Text = "Express Shipment";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(126, 1058);
+            this.label7.Location = new System.Drawing.Point(110, 990);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(93, 32);
+            this.label7.Size = new System.Drawing.Size(79, 29);
             this.label7.TabIndex = 13;
             this.label7.Text = "label7";
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(121, 1116);
+            this.textBox7.Location = new System.Drawing.Point(106, 1044);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(495, 38);
+            this.textBox7.Size = new System.Drawing.Size(434, 35);
             this.textBox7.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(126, 1200);
+            this.label8.Location = new System.Drawing.Point(110, 1123);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 32);
+            this.label8.Size = new System.Drawing.Size(79, 29);
             this.label8.TabIndex = 15;
             this.label8.Text = "label8";
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(121, 1265);
+            this.textBox8.Location = new System.Drawing.Point(106, 1183);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(495, 38);
+            this.textBox8.Size = new System.Drawing.Size(434, 35);
             this.textBox8.TabIndex = 16;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(126, 1345);
+            this.label9.Location = new System.Drawing.Point(110, 1258);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(93, 32);
+            this.label9.Size = new System.Drawing.Size(79, 29);
             this.label9.TabIndex = 17;
             this.label9.Text = "label9";
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(121, 1391);
+            this.textBox9.Location = new System.Drawing.Point(106, 1301);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(501, 38);
+            this.textBox9.Size = new System.Drawing.Size(439, 35);
             this.textBox9.TabIndex = 18;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(872, 268);
+            this.dataGridView1.Location = new System.Drawing.Point(763, 251);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 40;
-            this.dataGridView1.Size = new System.Drawing.Size(1171, 1035);
+            this.dataGridView1.Size = new System.Drawing.Size(1025, 968);
             this.dataGridView1.TabIndex = 19;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(1378, 1322);
+            this.label10.Location = new System.Drawing.Point(1206, 1237);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(148, 32);
+            this.label10.Size = new System.Drawing.Size(125, 29);
             this.label10.TabIndex = 20;
             this.label10.Text = ": Legend : ";
             this.label10.Click += new System.EventHandler(this.label10_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(2200, 670);
+            this.btnUpdate.Location = new System.Drawing.Point(1925, 627);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(393, 177);
+            this.btnUpdate.Size = new System.Drawing.Size(344, 166);
             this.btnUpdate.TabIndex = 21;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(2200, 977);
+            this.btnDelete.Location = new System.Drawing.Point(1925, 914);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(393, 177);
+            this.btnDelete.Size = new System.Drawing.Size(344, 166);
             this.btnDelete.TabIndex = 22;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(2200, 387);
+            this.btnInsert.Location = new System.Drawing.Point(1925, 362);
             this.btnInsert.Name = "btnInsert";
-            this.btnInsert.Size = new System.Drawing.Size(393, 177);
+            this.btnInsert.Size = new System.Drawing.Size(344, 166);
             this.btnInsert.TabIndex = 23;
-            this.btnInsert.Text = "Insert";
+            this.btnInsert.Text = "Hello";
             this.btnInsert.UseVisualStyleBackColor = true;
             // 
             // lblCurrentLoc
             // 
             this.lblCurrentLoc.AutoSize = true;
-            this.lblCurrentLoc.Location = new System.Drawing.Point(121, 755);
+            this.lblCurrentLoc.Location = new System.Drawing.Point(106, 706);
             this.lblCurrentLoc.Name = "lblCurrentLoc";
-            this.lblCurrentLoc.Size = new System.Drawing.Size(225, 32);
+            this.lblCurrentLoc.Size = new System.Drawing.Size(189, 29);
             this.lblCurrentLoc.TabIndex = 24;
             this.lblCurrentLoc.Text = "Current Location";
             // 
@@ -269,18 +269,18 @@
             // 
             this.gB1.Controls.Add(this.rbExprShipN);
             this.gB1.Controls.Add(this.rbExprShipY);
-            this.gB1.Location = new System.Drawing.Point(120, 940);
+            this.gB1.Location = new System.Drawing.Point(105, 879);
             this.gB1.Name = "gB1";
-            this.gB1.Size = new System.Drawing.Size(490, 100);
+            this.gB1.Size = new System.Drawing.Size(429, 94);
             this.gB1.TabIndex = 25;
             this.gB1.TabStop = false;
             // 
             // rbExprShipN
             // 
             this.rbExprShipN.AutoSize = true;
-            this.rbExprShipN.Location = new System.Drawing.Point(234, 37);
+            this.rbExprShipN.Location = new System.Drawing.Point(205, 35);
             this.rbExprShipN.Name = "rbExprShipN";
-            this.rbExprShipN.Size = new System.Drawing.Size(88, 36);
+            this.rbExprShipN.Size = new System.Drawing.Size(76, 33);
             this.rbExprShipN.TabIndex = 1;
             this.rbExprShipN.TabStop = true;
             this.rbExprShipN.Text = "No";
@@ -289,9 +289,9 @@
             // rbExprShipY
             // 
             this.rbExprShipY.AutoSize = true;
-            this.rbExprShipY.Location = new System.Drawing.Point(24, 37);
+            this.rbExprShipY.Location = new System.Drawing.Point(21, 35);
             this.rbExprShipY.Name = "rbExprShipY";
-            this.rbExprShipY.Size = new System.Drawing.Size(101, 36);
+            this.rbExprShipY.Size = new System.Drawing.Size(86, 33);
             this.rbExprShipY.TabIndex = 0;
             this.rbExprShipY.TabStop = true;
             this.rbExprShipY.Text = "Yes";
@@ -300,27 +300,27 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(1189, 1370);
+            this.label12.Location = new System.Drawing.Point(1040, 1282);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(140, 32);
+            this.label12.Size = new System.Drawing.Size(120, 29);
             this.label12.TabIndex = 26;
             this.label12.Text = "Inserted - ";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(1183, 1418);
+            this.label13.Location = new System.Drawing.Point(1035, 1327);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(146, 32);
+            this.label13.Size = new System.Drawing.Size(125, 29);
             this.label13.TabIndex = 27;
             this.label13.Text = "Updated - ";
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(1192, 1462);
+            this.label14.Location = new System.Drawing.Point(1043, 1368);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(137, 32);
+            this.label14.Size = new System.Drawing.Size(118, 29);
             this.label14.TabIndex = 28;
             this.label14.Text = "Deleted - ";
             // 
@@ -329,9 +329,9 @@
             this.InsertColor.AutoSize = true;
             this.InsertColor.BackColor = System.Drawing.Color.Aqua;
             this.InsertColor.ForeColor = System.Drawing.Color.Black;
-            this.InsertColor.Location = new System.Drawing.Point(1367, 1370);
+            this.InsertColor.Location = new System.Drawing.Point(1196, 1282);
             this.InsertColor.Name = "InsertColor";
-            this.InsertColor.Size = new System.Drawing.Size(358, 32);
+            this.InsertColor.Size = new System.Drawing.Size(307, 29);
             this.InsertColor.TabIndex = 29;
             this.InsertColor.Text = "                                                 ";
             // 
@@ -339,9 +339,9 @@
             // 
             this.UpdateColor.AutoSize = true;
             this.UpdateColor.BackColor = System.Drawing.Color.Lime;
-            this.UpdateColor.Location = new System.Drawing.Point(1368, 1417);
+            this.UpdateColor.Location = new System.Drawing.Point(1197, 1326);
             this.UpdateColor.Name = "UpdateColor";
-            this.UpdateColor.Size = new System.Drawing.Size(358, 32);
+            this.UpdateColor.Size = new System.Drawing.Size(307, 29);
             this.UpdateColor.TabIndex = 30;
             this.UpdateColor.Text = "                                                 ";
             // 
@@ -349,17 +349,17 @@
             // 
             this.DeletedColor.AutoSize = true;
             this.DeletedColor.BackColor = System.Drawing.Color.Red;
-            this.DeletedColor.Location = new System.Drawing.Point(1367, 1462);
+            this.DeletedColor.Location = new System.Drawing.Point(1196, 1368);
             this.DeletedColor.Name = "DeletedColor";
-            this.DeletedColor.Size = new System.Drawing.Size(358, 32);
+            this.DeletedColor.Size = new System.Drawing.Size(307, 29);
             this.DeletedColor.TabIndex = 31;
             this.DeletedColor.Text = "                                                 ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2740, 1535);
+            this.ClientSize = new System.Drawing.Size(2398, 1436);
             this.Controls.Add(this.DeletedColor);
             this.Controls.Add(this.UpdateColor);
             this.Controls.Add(this.InsertColor);
