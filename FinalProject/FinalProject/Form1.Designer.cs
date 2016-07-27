221bb02669e41fda8cfaea5b794c23bb8fdb4c82
PressKforKimchi <kevinleeorg@gmail.com>
2016-07-27 13:23:12 -0400

    stuff

--- FinalProject/FinalProject/Form1.Designer.cs
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -42,10 +42,6 @@
             this.lblExprShip = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label10 = new System.Windows.Forms.Label();
             this.btnUpdate = new System.Windows.Forms.Button();
@@ -177,38 +173,6 @@
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(495, 38);
             this.textBox7.TabIndex = 14;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(126, 1200);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 32);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "label8";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(121, 1265);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(495, 38);
-            this.textBox8.TabIndex = 16;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(126, 1345);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(93, 32);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "label9";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(121, 1391);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(501, 38);
-            this.textBox9.TabIndex = 18;
             // 
             // dataGridView1
             // 
@@ -373,10 +337,6 @@
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.lblExprShip);
@@ -418,10 +378,6 @@
         private System.Windows.Forms.Label lblExprShip;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button btnUpdate;
